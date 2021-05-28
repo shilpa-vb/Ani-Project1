0f203b61afa843fcc7078877a2320dc15d553a7b
shilpa-vb <70066434+shilpa-vb@users.noreply.github.com>
2021-05-28 18:51:36 +0530

    Update API_Interface.java

--- app/src/main/java/com/example/projectoneani/API_Interface.java
+++ app/src/main/java/com/example/projectoneani/API_Interface.java
@@ -7,10 +7,6 @@ public interface API_Interface {
 
     @GET("api/?key=21762589-eccf546162757c2653ac25ec6&q=nature&image_type=all")
 
- //   @GET("api/?key=21762589-eccf546162757c2653ac25ec6&q=animals&image_type=photo")
-
-//    @GET("api/?key=21762589-eccf546162757c2653ac25ec6&q=animals&image_type=photo&pretty=true")
-
 
     Call<JSON_Response> getGridImages();
 
